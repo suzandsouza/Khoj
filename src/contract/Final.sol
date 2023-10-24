ae11050683912f583e0fd1d3f96bfb1ae50a3d44
Martina John <martina07j@gmail.com>
2023-10-24 16:59:06 +0530

    contract changes

--- src/contract/Final.sol
+++ src/contract/Final.sol
@@ -19,39 +19,69 @@ contract ResearchFunding {
     mapping(address => Profile) public funders;
     FundingRequest[] public fundingRequests; // Changed to a single array
 
-    function registerResearcher(string memory _username, string memory _password) public {
+    function registerResearcher(
+        string memory _username,
+        string memory _password
+    ) public {
         researchers[msg.sender] = Profile(_username, _password, "researcher");
     }
 
-    function registerFunder(string memory _username, string memory _password) public {
+    function registerFunder(
+        string memory _username,
+        string memory _password
+    ) public {
         funders[msg.sender] = Profile(_username, _password, "funder");
     }
 
-    function authenticate(address _address, string memory _username, string memory _password) public view returns (bool) {
+    function authenticate(
+        address _address,
+        string memory _username,
+        string memory _password
+    ) public view returns (bool) {
         Profile memory researcher = researchers[_address];
         Profile memory funder = funders[_address];
 
-        if (keccak256(abi.encodePacked(researcher.entity)) == keccak256(abi.encodePacked("researcher")) &&
-            keccak256(abi.encodePacked(researcher.username)) == keccak256(abi.encodePacked(_username)) &&
-            keccak256(abi.encodePacked(researcher.password)) == keccak256(abi.encodePacked(_password))) {
+        if (
+            keccak256(abi.encodePacked(researcher.entity)) ==
+            keccak256(abi.encodePacked("researcher")) &&
+            keccak256(abi.encodePacked(researcher.username)) ==
+            keccak256(abi.encodePacked(_username)) &&
+            keccak256(abi.encodePacked(researcher.password)) ==
+            keccak256(abi.encodePacked(_password))
+        ) {
             return true;
         }
 
-        if (keccak256(abi.encodePacked(funder.entity)) == keccak256(abi.encodePacked("funder")) &&
-            keccak256(abi.encodePacked(funder.username)) == keccak256(abi.encodePacked(_username)) &&
-            keccak256(abi.encodePacked(funder.password)) == keccak256(abi.encodePacked(_password))) {
+        if (
+            keccak256(abi.encodePacked(funder.entity)) ==
+            keccak256(abi.encodePacked("funder")) &&
+            keccak256(abi.encodePacked(funder.username)) ==
+            keccak256(abi.encodePacked(_username)) &&
+            keccak256(abi.encodePacked(funder.password)) ==
+            keccak256(abi.encodePacked(_password))
+        ) {
             return true;
         }
 
         return false;
     }
 
-    function listFundingRequests() public view returns (FundingRequest[] memory) {
+    function listFundingRequests()
+        public
+        view
+        returns (FundingRequest[] memory)
+    {
         return fundingRequests; // Return all funding requests
     }
 
     function makeFundingRequest(string memory _title, uint256 _amount) public {
-        require(keccak256(abi.encodePacked(researchers[msg.sender].entity)) == keccak256(abi.encodePacked("researcher")), "Only researchers can make funding requests");
-        fundingRequests.push(FundingRequest(msg.sender, _title, _amount, false)); // Add to the general list
+        require(
+            keccak256(abi.encodePacked(researchers[msg.sender].entity)) ==
+                keccak256(abi.encodePacked("researcher")),
+            "Only researchers can make funding requests"
+        );
+        fundingRequests.push(
+            FundingRequest(msg.sender, _title, _amount, false)
+        ); // Add to the general list
     }
 }
